2e09aded42cf9f00afcbc8339ae27ea9dda94a0a
Noel-Cruz <126811536+Noel-Cruz@users.noreply.github.com>
2024-01-08 01:28:07 +0000

    septimo commit

--- Venex/src/componentes/ItemDetail/ItemDetail.jsx
+++ Venex/src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,8 +1,21 @@
 
-const ItemDetail = () => {
+const ItemDetail = ({img,titulo,precio,descripcion}) => {
   return (
-    <article>
-        
+    <article id='ItemDetail'>
+        <picture>
+          <img src={img} alt="" />
+        </picture>
+        <div>
+          <h5>
+            {titulo}
+          </h5>
+          <span>
+            {precio}
+          </span>
+          <p>
+            {descripcion}
+          </p>
+        </div>
     </article>
   )
 }
--- Venex/src/componentes/ItemDetailContainer/ItemDetailContainer.jsx
+++ Venex/src/componentes/ItemDetailContainer/ItemDetailContainer.jsx
@@ -7,12 +7,7 @@ const ItemDetailContainer = () => {
     const [producto,setProducto]=useState([]);
     const {id}=useParams();
 
-  function productoDetail(){
-    DBProductos().then((response)=>{
-       const producto=response.find((producto)=>producto.id===id);
-       setProductos(producto);
-    })
-  }
+    
   useEffect(()=>{
     DBProductos().then((response)=>{
         const producto=response.find((producto)=>producto.id===id);
